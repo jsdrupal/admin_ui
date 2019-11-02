13dd7220ebc239020260ae34958ce38a8c3d42e0
Sally Young <sally@justafish.co.uk>
2019-11-02 10:56:05 +0000

    [#13] Adds prettier, prettier test, and a .editorconfig file

--- vfancy/src/index.js
+++ vfancy/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Example from './component/Example';
 
-const App = () => (
-  <Example name="world" />
-);
+const App = () => <Example name="world" />;
 
 ReactDOM.render(<App />, document.getElementById('root'));
