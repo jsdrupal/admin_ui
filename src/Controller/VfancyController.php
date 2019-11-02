8eda7ae43f8e712de37726e1f7a4e42157448abf
Sally Young <sally@justafish.co.uk>
2019-11-02 00:46:24 +0000

    [#9] Remove some boilerplate from the controller

--- src/Controller/VfancyController.php
+++ src/Controller/VfancyController.php
@@ -13,12 +13,8 @@ class VfancyController extends ControllerBase {
      * Returns the contents of dist/index.html.
      */
     public function content() {
-        $build = [
-            '#markup' => $this->t('Hello World!'),
-        ];
         $file = file_get_contents(__DIR__ .'/../../dist/index.html');
         $file = str_replace('MODULE_LOCATION', base_path() . drupal_get_path('module', 'admin_ui'), $file);
         return new Response($file, 200);
-        return $build;
     }
 }
